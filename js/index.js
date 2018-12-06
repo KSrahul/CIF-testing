cd4205c0a744ea509803f27730ce14cb53bca7c6
KSrahul <rahul@ampersanddesign.io>
2018-12-06 20:21:20 +0530

    Change done after after updation
    Change done after after updation

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ window.onload = function() {
     // trying to getting the current id and minus header height so that scroll reach exact position
     function navLinks() {
         var ids = (this).getAttribute("href");
-        if ($(window).width() > 716) {
+        if ($(window).width() > 768) {
             $("html,body").animate({
                 scrollTop: $(ids).offset().top - 80
             }, 500);
@@ -22,10 +22,10 @@ window.onload = function() {
     // trying to getting the current id and minus header height so that scroll reach exact position (End)
 
 
-    // Try to geeting, that if screen size is less than 716 that only this code will be work
+    // Try to geeting, that if screen size is less than 768 that only this code will be work
 
     // On scroll navigation highlited or active if reach to perticular section
-    if ($(window).width() > 716) {
+    if ($(window).width() > 768) {
         $(window).scroll(function() {
             var windscroll = $(window).scrollTop() + 80
             $(".contentStart .pageSection").each(function(i) {
@@ -58,10 +58,10 @@ window.onload = function() {
             // If scroll reach to header on the banner then header should be fixed (End)
         }).scroll();
     }
-    // Try to geeting, that if screen size is less than 716 that only this code will be work (End)
+    // Try to geeting, that if screen size is less than 768 that only this code will be work (End)
 
 
-    if ($(window).width() < 716) {
+    if ($(window).width() < 768) {
         function topScroll() {
             if (window.pageYOffset >= 546) {
                 document.querySelector(".backTop").classList.add("showBackTop")
@@ -78,11 +78,9 @@ window.onload = function() {
             }, 500);
         })
     }
-
-
     // window.addEventListener("resize", function() {
-    //     if (window.matchMedia("(max-width: 716px)").matches) {
-    //         if ($(window).width() < 716) {
+    //     if (window.matchMedia("(max-width: 768px)").matches) {
+    //         if ($(window).width() < 768) {
     //             location.reload();
     //         } else {
     //             location.reload();
